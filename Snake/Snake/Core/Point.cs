64c06fc2634369699681cea3da5bbca87ec73b92
CsharpdeveloperInt <csharpdeveloper@list.ru>
2017-02-09 14:38:14 +0600

    Добавлен новый проект

---
+++ Snake/Snake/Core/Point.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Snake.Core
+{
+    internal class Point
+    {
+        public int X;
+        public int Y;
+        public char Sym;
+
+        public void Draw()
+        {
+            Console.SetCursorPosition(X,Y);
+            Console.Write(Sym);
+        }
+    }
+}
---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Snake.Core;
+
+namespace Snake
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var point1 = new Point
+            {
+                X = 8,
+                Y = 10,
+                Sym = '#'
+            };
+            point1.Draw();
+
+            var point2 = new Point
+            {
+                X = 12,
+                Y = 32,
+                Sym = '^'
+            };
+
+            point2.Draw();
+            Console.ReadLine();
+        }
+    }
+}
